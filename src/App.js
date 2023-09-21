a8117e2e0226d3fa5d2a1f7e8d09d1d82f609034
Katarzyna Zielinska <jekaterina.zielinska@gmail.com>
2023-09-21 17:33:35 +0200

    initialize git repository

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Inspiring quote</h1>
+      <Quote text="To be, or not to be..." author="Hamlet" />
+    </div>
+  );
+}
+
+function Quote({ text, author }) {
+  return (
+    <blockquote>
+      {text}
+      <footer>{author}</footer>
+    </blockquote>
+  );
+}
